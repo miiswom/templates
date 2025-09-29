67f88937cd0e58a163dee035de04f26342410ab4
Farmata Kane <kane.farmata@gmail.com>
2025-09-29 12:27:13 +0100

    Simplify and improve  modal

--- modal/main.js
+++ modal/main.js
@@ -1,31 +1,11 @@
-const addNewBook = document.querySelector("#add-new");
-const modal = document.querySelector("#modal");
-const modalX = document.querySelector("#modal-close");
-const modalClearButton = document.querySelector("#clear-button");
-const addButton = document.querySelector("#add-button");
+const modal = document.querySelector('#modal');
+const closeBtn = document.querySelector('#closeModal');
+const openBtn = document.querySelector('#openModal');
 
+openBtn.addEventListener('click', () => {
+  modal.classList.add('open');
+});
 
-addNewBook.addEventListener("click", openModal);
-modalX.addEventListener("click", closeModal);
-modalClearButton.addEventListener("click", clearModalValues);
-addButton.addEventListener("click", addBookToLibrary);
-
-function openModal() {
-  modal.style.display = "block";
-  mainContainer.style.filter = "blur(5px)";
-}
-
-function closeModal() {
-  modal.style.display = "none";
-  mainContainer.style.filter = "blur(0px)"
-}
-
-function clearModalValues() {
-  newBookTitle.value = "";
-  newBookAuthor.value = "";
-  newBookPages.value = "";
-  newBookLang.value = "";
-  newBookPubs.value = "";
-  newBookIsRead.checked == false;
-  newBookIsUnread.checked == false
-}
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('open');
+});
